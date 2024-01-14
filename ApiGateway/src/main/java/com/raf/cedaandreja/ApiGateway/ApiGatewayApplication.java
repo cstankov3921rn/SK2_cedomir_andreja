085df5dd1883c80071a84ba6356168cc0c96788f
Andreja Radosavljevic 35/21 RN <aradosavljevic3521rn@raf.rs>
2024-01-14 19:01:39 +0100

    Pocetak fronta #5

--- ApiGateway/src/main/java/com/raf/cedaandreja/ApiGateway/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/com/raf/cedaandreja/ApiGateway/ApiGatewayApplication.java
@@ -4,10 +4,16 @@ package com.raf.cedaandreja.ApiGateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@CrossOrigin(origins = "http://localhost:8000")
 public class ApiGatewayApplication {
+
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
