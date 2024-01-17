6b8885c14aa31fdf6dec4b3f2526fb2e882949c2
Andreja Radosavljevic 35/21 RN <aradosavljevic3521rn@raf.rs>
2024-01-17 03:10:56 +0100

    Pocetak fronta #16

--- NotifikacioniServis/src/main/java/com/raf/cedaandreja/NotifikacioniServis/configuration/EmailConfiguration.java
+++ NotifikacioniServis/src/main/java/com/raf/cedaandreja/NotifikacioniServis/configuration/EmailConfiguration.java
@@ -23,8 +23,8 @@ public class EmailConfiguration {
         mailSender.setPort(587);
 
         //TODO kad hoces da testiras moras da ubacis ovde neki mail kredencijale. Napravicu nam privremeni mail kasnije
-        mailSender.setUsername("aaaaaaaaaaa");//ovde ide mail
-        mailSender.setPassword("aaaaaaaaaaaa");//ovdde sifra mail-a
+        mailSender.setUsername("aaaaaaaa");//ovde ide mail
+        mailSender.setPassword("aaaaaaaa");//ovdde sifra mail-a
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
